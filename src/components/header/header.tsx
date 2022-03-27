d18e96770cc679805849a20e12541ba75de286b8
Ziphil <yfe23457@nifty.com>
2022-03-27 17:35:31 +0900

    Update

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,6 +2,7 @@ import { Box, Button, Flex, FlexProps, IconButton } from "@chakra-ui/react"
 import { ReactNode } from "react"
 import NextLink from "next/link"
 import { MdPerson } from "react-icons/md"
+import { useRouter } from "next/router"
 
 
 type Props = {
@@ -9,9 +10,13 @@ type Props = {
 
 export const Header: React.FC<Props> = ({
 }) => {
+  const router = useRouter()
+
   return (
     <Flex h={12} px={4} background="background.pale" align="center" justify="space-between">
-      <Box fontFamily="title" fontSize="2xl" fontWeight="900">mame</Box>
+      <Box fontFamily="title" fontSize="2xl" fontWeight="900" onClick={() => router.push("/")}>
+        mame
+      </Box>
       <Box>
         <NextLink href="/profile" passHref={true}>
           <IconButton size="sm" icon={<MdPerson/>} variant="ghost" color="text.gray" aria-label="profile"/>
